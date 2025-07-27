604e1f5491970d81400abecef729c2658d1bd141
tharakeshrajesh <tharakeshrajesh3@gmail.com>
2025-07-27 17:18:47 -0500

    Update main.py
    Textual sucks, I switched to rich and it works

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
-from textual.app import App, ComposeResult
-from textual.widgets import Label, Header, Footer
-from ascii_magic import AsciiArt
+from rich import print as rprint
+import numpy as np
+import pymunk, cv2, numpyasciiart
 
-plane = AsciiArt.from_image('plane.png').to_file("plane.txt", monochrome=True)
-with open("plane.txt", "r") as file:
-    plane = file.read()
+space = pymunk.Space()
+space.gravity = (0, 500)
+render = np.zeros((512, 512, 3), dtype=np.uint8)
+apples = []
 
-class Sim(App):
-    TITLE = "Flight Physics Simulator"
+def create_apples(space):
+    body = pymunk.Body(1, 100, pymunk.Body.DYNAMIC)
+    body.position = (400, 0)
+    shape = pymunk.Circle(body, 20)
+    space.add(body, shape)
+    return shape
 
-    def compose(self) -> ComposeResult:
-        yield Header()
-        yield Footer()
-        yield Label(plane)
+def draw_apples(apples):
+    render[:] = 0
+    for apple in apples:
+        cv2.circle(render, tuple(map(int, apple.body.position)), 20, (255,255,255), 4)
+        rprint(numpyasciiart.to_ascii(render, 100, 2.5, " ░░▒▒▓▓█"))
 
-if __name__ == "__main__":
-    app = Sim()
-    app.run()
+apples.append(create_apples(space))
+
+while True:
+    draw_apples(apples)
+    space.step(0.02)
+    cv2.waitKey(1)
